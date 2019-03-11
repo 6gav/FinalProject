c8dbef5a514f751c98f63fdcab6a485dde8d2b78
Sabian McElroy <SGMcElroy@student.fullsail.edu>
2019-03-11 18:35:59 -0400

    hasAuthUser func returning false until user accounts integration

--- client/src/components/layouts/Header.js
+++ client/src/components/layouts/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { link } from 'fs';
 
-export default function Header() {
+export default function Header(props) {
     const linkStyle = {
         color:'#fff',
         textDecoration: 'none'
@@ -13,14 +13,15 @@ export default function Header() {
         textAlign: 'center',
         padding: '10px',
     }
-    const authuser = this.props.hasAuthuser()
+    const authuser = props.hasAuthuser()
   return (
       
     <header style={headerStyle}>
         <h1>Final Project</h1>
         <Link style={linkStyle} to="/">Home</Link> |
-        <Link style={linkStyle} to="/about">About</Link> |
-        <Link styel={linkStyle} to={authuser?"/profile":"/signin"}>{authuser?"My Profile":"Sign in"}</Link>
+        <Link style={linkStyle} to="/about"> About</Link> |
+        <Link style={linkStyle} to={authuser?"/profile":"/signin"}>{authuser?" My Profile":" Sign in"}</Link>
+        
         
     </header>
   )
