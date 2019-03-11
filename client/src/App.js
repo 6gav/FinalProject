9a0f8b214a3789c5682947d10423e2283bce51cd
Sabian McElroy <SGMcElroy@student.fullsail.edu>
2019-03-11 17:40:50 -0400

    Base frontend commit, including simple Home page, and Base Grid for visualizing simulation

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home.js';
+import Grid from './components/Grid.js';
+import {Switch, Route,Router} from 'react-router-dom';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.p = this;
+  }
+  onPositionClick = (event) => {
+    let message = "You clicked on cell ["+event.x+", "+event.y+"]"
+    this.setState({
+      clickPos:event,
+      message:message,
+      msgId: this.state.msgId+1,
+    })
+    
+  }
+
   render() {
+    const Ap = () => (
+      <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/Game' component={Grid} onPositionClick={this.onPositionClick}/>
+        </Switch>
+    );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Route>
+          <Ap/>
+        </Route>
     );
   }
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import '../components/Home.css'
+class Home extends Component{
+//#region colors
+/*
+Reds
+E55041
+401612
+FF5948
+7F2C24
+BF4336
+
+Blues
+5D68BF
+3E457F
+7C8AFF
+1F2340
+707CE5
+*/
+//#endregion
+    BeginSinglePlayer = (event) =>{
+        //
+    }
+    Modes = (event) =>{
+
+    }
+    render(){
+        //Holds "buttons" that 
+        const Lnkbtn = (props) =>
+        {
+            return (
+            <div className = 'LinkContainer'>
+            <a className="button" href = {props.href}>{props.text}</a>
+            </div>
+            );
+        }
+        return(
+            <div >
+                <div className="Page">
+                    <div className="Title">DeGaS Final Project</div>
+                    <div className="TitleMenuContainer">
+                        <Lnkbtn href="/Game" id="SinglePlayer" onClick={this.BeginSinglePlayer}  text="SinglePlayer"></Lnkbtn>
+                        <Lnkbtn href="/Game" id="SinglePlayer" onClick={this.Modes} text='Multiplayer Modes'></Lnkbtn>
+                        <Lnkbtn href="/Game" id="Cell Selection" text ="Cells"></Lnkbtn>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
