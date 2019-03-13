44112a268bdb96bf1b3f74aefa69289a8cc32a18
gavset99@gmail.com <blackcat99>
2019-03-13 10:58:26 -0400

    Added primitive firebase user saving

--- server/api.js
+++ server/api.js
@@ -1,7 +1,32 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccount.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://temp-sim-database.firebaseio.com',
+});
+
+var auth = admin.auth();
+
 module.exports = {
     
+
+
     registerUser: function(user){
         console.log(user);
+        
+        auth.createUser({
+            email: user.email,
+            password: user.password,
+            displayName: user.username
+        });
+
+    },
+
+
+    loginUser: function(user){
+        console.log(user);
+
     }
 
 }
