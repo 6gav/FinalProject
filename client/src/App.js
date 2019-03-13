3173e3add5297c8a78cb5a6ad91886a1b5dd4b8f
Sabian McElroy <SGMcElroy@student.fullsail.edu>
2019-03-13 10:03:03 -0400

    Const App Version added

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,9 @@ import Home from './components/pages/Home.js';
 import Grid from './components/Grid.js';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
-
+const version = "v0.0.2."
 class App extends Component {
-
+  
   constructor(props){
     super(props)
     this.p = this;
@@ -22,18 +22,20 @@ class App extends Component {
     })
     
   }
-
+  hasAuthuser = () => {
+    return false
+  }
   render() {
     return (
       
       <Router>
         <div className = "App">
           <div className="Container">
-            <Header/>
+            <Header hasAuthuser={this.hasAuthuser}/>
             <Switch>
               <Route exact path='/' component={Home}/>
               <Route exact path='/game' component={Grid} onPositionClick={this.onPositionClick}/>
-              <Route exact path='/about'/>
+              <Route exact path='/about' component={About}/>
             </Switch>
           </div>
         </div>
