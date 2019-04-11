23d741eb628bbbde23f5babafedbcfe0cc9dcd23
Sabian McElroy <SGMcElroy@student.fullsail.edu>
2019-04-11 09:24:28 -0400

    Zombie Code Removal Dummy api post with user choice

--- client/src/components/layouts/ChoicePrompt.js
+++ client/src/components/layouts/ChoicePrompt.js
@@ -3,17 +3,49 @@ import './ChoicePrompt.css'
 export default class ChoicePrompt extends Component {
 
     handleClick = (choice)=>{
-        //this.props.choice(target)
-        if(choice.event){
-          choice.event()
-        }else{
-          console.log("No ev found")
-          console.log(choice)
+        if(this.props.choice.callback){
+          this.props.choice.callback()
+          //console.log(`You clicked on: ${choice.Action.text}`)
+        }
+        if(this.props.OnChoice){
+          this.props.OnChoice(choice)
+        }
+        if(choice.Action)
+          {
+            //console.log(this.props)
+          let b = true;
+          if(b){
+            
+            fetch('/api/responsePrompt', {
+              method: 'POST',
+              headers:{
+                'Content-Type':'Application/json',
+              },
+              body: JSON.stringify({
+                choice:choice
+              })
+            }).then(function(response) {
+              return response.json();
+              
+            }).then(function(json) {
+              console.log(json)
+            }).catch(function(error) {
+              console.log(error);
+            });
+          }
+            if(choice.event){
+              
+              console.log("ev found")
+              choice.event()
+            }
+          else
+          {
+            //console.log(choice)
+          }
         }
     }
   render() {
     let choices = this.props.choice.choices
-      console.log(this.props)
     return (
       <div className="Prompt">
         <form >
@@ -23,16 +55,10 @@ export default class ChoicePrompt extends Component {
                 <div>
                     
                     {
+                      
                         choices.map((choice) =>{
                         return <input name="prompt_button" style={{backgroundColor:"darkcyan",textAlign:"center",margin:`5% ${choices.length==1?'30':'5'}%`,padding:"5% 8%", width:"40%"}} type="button" value={choice.Action.text} onClick={()=>{this.handleClick(choice);}}/>
-                        /*
-                        .PromptButton{
-    background-color: darkcyan;
-    text-align: center;
-    margin:  5%;
-    padding: 5% 8%;
-    width: 40%;
-} */
+                        
                     })}
 
                     </div>
