9b38c364b50b08e4c30f98b8f1bbf5596980da96
Sabian McElroy <SGMcElroy@student.fullsail.edu>
2019-04-11 22:31:21 -0400

    Primary App Modifications

--- client/src/App.js
+++ client/src/App.js
@@ -8,30 +8,41 @@ import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import { Registration } from './components/pages/Registration';
 import Debugger from "./components/layouts/Debugger"
 import Interface from './components/layouts/Interface';
-
-
+//TODO: revise code.
+//TODO: Cell Personalize objects
+//cowboy hat leather chaps
 const uuidv1 = require('uuid/v1');
-const version = "v0.0.4."
+const version = "v0.1.3."
+
 
 const PromptOneChoice=(message,event=null)=>{
   let msg = {
+    callback:event,
     question:message,
-    choices:[{Action:{text:"Continue",event:event}}],
+    choices:[{Action:{text:"Continue"}}],
     answer:null
     }
   return msg;
 }
-const PromptMultipleChoice = (caption,messages,events=null)=>{
+const PromptMultipleChoice = (caption,messages,event)=>{
   let choices = [];
   messages.map((message) =>{
     choices.push({Action:{text:message}})
   })
-  if(events){
-    events.map((event) =>{
-      choices.push({Action:{event:event}})
-    })
+  /*
+  console.log("hiyaaa")
+  console.log(caption)
+  console.log(messages)
+  console.log(event)
+ */
+  if(event){
+    for (let i = 0; i < choices.length; i++) {
+      choices[i].Action.event = event
+    }
+    console.log(choices.length)
   }
   return {
+    callback:event,
     question:caption,
     choices:choices,
     answer:null
@@ -43,15 +54,20 @@ class App extends Component {
     gridSize:30,
     gridSpacing:24,
     showDebug:true,
+    user:null,
     messages:[],//array of message objects. objects have an id, along with data containing the string of the message.
-    choice:PromptOneChoice("Cee",this.onClosePrompt),
+    choice:null,//,PromptOneChoice("Press the button to begin.",this.onClosePrompt),
   }
   
   
   constructor(props){
     super(props)
     this.p = this;
-    this.state.choice.choices[0].event = this.onClosePrompt
+    this.state.choice = PromptMultipleChoice("What's your favorite color","Red,Blue,Green,Yellow,beige,darkmagenta,tomato,white,wheat,limegreen,peachpuff,salmon".split(','),this.onClosePrompt)
+    //this.state.choice.choices[0].event = this.onClosePrompt
+  }
+  OnChoice = (target) =>{
+    console.log("You hit one of the choices")
   }
   onClosePrompt = (event) =>{
     //event.preventDefault()
@@ -78,33 +94,41 @@ class App extends Component {
     
 
   }
-
+  MakeChoice = (choice) => {
+    choice.callback = this.onClosePrompt
+    this.setState({choice:choice})
+  }
   onDebugValuesChange = (data)=>{
     console.log(data)
     //this.setState({gridSize:data.gridSize,gridSpacing:data.gridSpacing})
     let gridSize = data.gridSize
     let gridSpacing = data.gridSpacing
 
-    gridSize = gridSize<15?15:gridSize
+    gridSize = gridSize<12?12:gridSize
     gridSize = gridSize>70?70:gridSize
     gridSpacing = gridSpacing<5?5:gridSpacing
-    gridSpacing = gridSpacing>30?30:gridSpacing
+    gridSpacing = gridSpacing>100?100:gridSpacing
     
     this.state.gridSize = gridSize
     this.state.gridSpacing = gridSpacing
     this.setState(this.state)
     console.log(this.state)
   }
-  OnChoice = (target) =>{
-    console.log("You hit one of the choices")
-  }
+  
   render() {
     const SinglePlayerGame= (<div className="GameSpace">
-      <Grid choice={this.state.choice} OnChoice={this.OnChoice} gridSize={this.state.gridSize} gridSpacing={this.state.gridSpacing} onPositionClick={this.onPositionClick}></Grid></div>);
-    const MultiPlayerGame = (<div className="GameSpace">
-      <Grid choice={this.state.choice} OnChoice={this.OnChoice} gridSize={this.state.gridSize} gridSpacing={this.state.gridSpacing} onPositionClick={this.onPositionClick}></Grid>
-      <Interface onDebugValuesChange={this.onDebugValuesChange} messages={this.state.messages} AddMessage={this.AddMessage}></Interface></div>);
+          <Grid onClosePrompt={this.onClosePrompt}choice={this.state.choice} OnChoice={this.OnChoice} gridSize={this.state.gridSize} gridSpacing={this.state.gridSpacing} onPositionClick={this.onPositionClick}></Grid></div>);
+        const MultiPlayerGame = (<div className="GameSpace">
+        <Grid choice={this.state.choice} OnChoice={this.OnChoice} gridSize={this.state.gridSize} gridSpacing={this.state.gridSpacing} onPositionClick={this.onPositionClick}></Grid>
+          <Interface onDebugValuesChange={this.onDebugValuesChange} messages={this.state.messages} AddMessage={this.AddMessage}></Interface></div>);
+          const CustomizeCell = (<div>
+            <Grid choice={this.state.choice} OnChoice={this.OnChoice} gridSize={30} gridSpacing={40} onPositionClick={this.onPositionClick}></Grid>
+            </div>
+            )
     
+
+
+    let MakeChoice = this.MakeChoice
     return (
       
       <Router>
@@ -114,13 +138,14 @@ class App extends Component {
             <Switch>
               <Route exact path='/' component={Home}/>
               <Route exact path='/simulation_s' render={() =>SinglePlayerGame} />
-              <Route exact path='/simulation_m' render={() =>MultiPlayerGame} />
+              <Route exact path='/simulation_m' render={() =>MultiPlayerGame}  />
+              <Route exact path='/simulation_editor' render={()=>CustomizeCell}/>
               <Route exact path='/about' render={()=><About props={{version:version}}/>} />
               <Route exact path='/signin' render={()=><Registration props={{version:version}}/>}/>
             </Switch>
             {/*
-            */}
-            <Debugger gridSize={this.state.gridSize} gridSpacing={this.state.gridSpacing} AddMessage={this.AddMessage} onDebugValuesChange={this.onDebugValuesChange}></Debugger>
+          */}
+            <Debugger MakeChoice={MakeChoice} PromptMultipleChoice={PromptMultipleChoice} PromptOneChoice={PromptOneChoice} gridSize={this.state.gridSize} gridSpacing={this.state.gridSpacing} AddMessage={this.AddMessage} onDebugValuesChange={this.onDebugValuesChange}></Debugger>
             
           </div>
         </div>
