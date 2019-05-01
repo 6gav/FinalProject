e43d4f88ffe5626ddce88661350e55151533b11a
Sabian McElroy <SGMcElroy@student.fullsail.edu>
2019-05-01 17:34:34 -0400

    Updated About Page Layout

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-export default function About() {
+export default function About(props) {
+  let version = props.version;
   return (
     <React.Fragment>
     <h1>About</h1>
+    <h2>
+      Current version: {version}
+    </h2>
     <p>
-        The purpose of this application is to serve as
-        the Full Sail University capstone project for 
-        the authors of the project. current version v0.0.2.
+      The purpose of this application is to serve as
+      the Full Sail University capstone project for 
+      the authors of the project. 
     </p>
     </React.Fragment>
 
