b1531732a8ae43e7ca6297a731f56784b4841d90
Sabian McElroy <SGMcElroy@student.fullsail.edu>
2019-05-01 17:02:56 -0400

    Beginning conversion of cell to char convention

--- client/src/components/layouts/resources/Cell.js
+++ client/src/components/layouts/resources/Cell.js
@@ -5,6 +5,7 @@ import cell_red from './cell/color_mask_00.png'
 import cell_blue from './cell/color_mask_09.png'
 import cell_green from './cell/color_mask_05.png'
 import cell_purp from './cell/color_mask_11.png'
+
 import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
 let cell_color=null;
 export class Cell extends Component {
@@ -17,19 +18,20 @@ export class Cell extends Component {
     }
     constructor(props){
         super(props)
-        
-        this.state.color = props.color?props.color:null;
-        this.state.face = props.face?props.face:null;
-        this.state.imgs = props.imgs?props.imgs:[];
-        this.state.dimensions = props.dimensions?props.dimensions:this.state.dimensions
     }
     
     render(){
+        let char = this.props.char?this.props.char:null
         let color = (this.props.color?this.props.color:this.state.color)
-        
-        let spacing = this.state.dimensions.Spacing*0.8,rad=-2,
-        translation = 'translate('+rad+'px, '+rad+'px)',
+        if(char){
+            let color = char.body.color
+            let face = char.body.face
+            
+        }
+        let spacing = this.props.dimensions.Spacing*0.8,rad=-2,
+        translation='translate('+rad+'px, '+rad+'px)',
         invlation = 'translate('+(-rad)+'px, '+(-rad)+'px)';
+        return <div/>
         return(
             <div className="Cell" style={{
                 transform:invlation,
