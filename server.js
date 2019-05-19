9728afb9943e0a502e661442b99c41371fd96cd2
gavset99@gmail.com <blackcat99>
2019-05-19 12:03:08 -0400

    Login now sends user back

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io');
+const io = require('socket.io').listen(server);
 
+io.on('connection', (client) => {
+    console.log(client);
+});
 
 //Server setup
 app.use(bodyParser.json());
