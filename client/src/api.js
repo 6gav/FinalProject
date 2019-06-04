c5a63384c02bdb5da3b894bc814fee3e9e74812f
Sabian McElroy <SGMcElroy@student.fullsail.edu>
2019-06-03 20:36:12 -0400

    socket api base setup

--- client/src/api.js
+++ client/src/api.js
@@ -1,28 +1,45 @@
 import io from 'socket.io-client';
 const socket = io();
-let map = null;
-function connectToSocket(cb){
-    console.log('connecting');
-    socket.on('acceptConnection', vals => cb(null, vals));    
-};
 
-function disconnectFromServer(){
-    console.log('Disconnecting.');
-    socket.emit('disconnect');
+class SocketApi{
+    constructor(){
+        this.map_cb = null;
+
+        socket.on('map',(map)=>{
+            map = map;
+        
+            if(map_cb != null){
+                map_cb(map)
+            }
+        })
+
+    }
+    connectToSocket(cb){
+        console.log('connecting');
+        socket.on('acceptConnection', vals => cb(null, vals));    
+    };
+
+    disconnectFromServer(){
+        console.log('Disconnecting.');
+        socket.emit('disconnect');
+    }
+    //get game map. data includes current user uid, and game ID 
+    getMap(data,cb){
+        console.log('fetching map data')
+        socket.emit('getMap',data)
+        
+        map_cb = cb;
+    }
+
+    sendUid(uid){
+        socket.emit('uid',uid);
+    }
+
+
+
+    //data: object containing info aout render location - top left corner, width, and height of grid
+    getCells(data){
+        socket.emit("getCells")
+    }
 }
-
-function getMap(data,cb){
-    console.log('fetching map data')
-    socket.emit('getMap',data)
-    
-    
-}
-
-function sendUid(uid){
-    socket.emit('uid',uid);
-}
-
-socket.on('map',(json)=>{
-    map = json.map;
-})
-export {connectToSocket, disconnectFromServer, sendUid, getMap};
+module.exports = SocketApi;
