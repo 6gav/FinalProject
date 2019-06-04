0b457d86a19d05dba204f0367c394137e314324c
Sabian McElroy <SGMcElroy@student.fullsail.edu>
2019-06-03 20:02:52 -0400

    Home.js Changes

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './Home.css'
-import RefButton from './tools/RefButton'
 
 class Home extends Component{
 //#region colors
@@ -20,24 +19,27 @@ Blues
 */
 
 //#endregion
+
     BeginSinglePlayer = (event) =>{
+        window.location=event.target.getAttribute("href")
         this.props.CreateGame()
-        //multiplayer is simply a publicly initialized game  
+        //multiplayer is simply a publicly initialized game
     }
     Modes = (event) =>{
 
     }
     render(){
-        
-        
         return(
             <div >
                 <div className="Page">
                     <div className="Title">DeGaS Final Project</div>
                     <div className="TitleMenuContainer">
+                        {/*
                         <RefButton name="btn_singleplayer" href="/simulation" id="SinglePlayer" onClick={this.BeginSinglePlayer}  text="SinglePlayer"></RefButton>
-                        <RefButton name="btn_multiplayer" href="/simulation" id="MultiPlayer" onClick={this.Modes} text='Multiplayer Modes'></RefButton>
-                        <RefButton name="btn_editor" href="/simulation_editor" id="Cell Selection" name="btn_home_cell_selection"text ="Cells"></RefButton>
+                        */}
+                        <button className="LinkContainer" name="btn_singleplayer" href="/simulation" id="SinglePlayer" onClick={this.BeginSinglePlayer} >Run Simulation</button>
+                        <button className="LinkContainer" name="btn_editor" href="/simulation_editor" id="Cell Selection" onClick={this.BeginSinglePlayer} Click={()=>{window.location="/simulation_editor"}}name="btn_home_cell_selection" text="Cells">View Cells</button>
+                        
                     </div>
                 </div>
             </div>
